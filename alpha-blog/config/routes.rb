97dc68591fb902cc4917bfe17381c787c471f3f1
Peter Chen <peterchen@Peters-Mac-mini.local>
2019-05-11 20:01:51 -0400

    added page controller and views

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get 'pages/home', to: 'pages#home'
+
+  get 'pages/about', to: 'pages#about'
 end
